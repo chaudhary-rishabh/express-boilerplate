f323da8908f7160fda49cfb1b516a97e91264cbe
chaudhary-rishabh <chaudharyrishabh029@gmail.com>
2025-08-08 18:28:06 +0530

    changed verbatimModuleSyntax to false

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
-import { AnyZodObject } from 'zod';
+import { ZodObject } from 'zod';
 
-export const validate = (schema: AnyZodObject) => (
+export const validate = (schema: ZodObject) => (
   req: Request,
   res: Response,
   next: NextFunction
